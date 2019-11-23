671ba588045a08cb70ab3fb09e024bea27ab0088
Marni Anuradha <marnianuradha@achiever-111.local>
2019-11-23 11:52:20 +0530

    Initial Commit

---
+++ DataTransferThruNotificationCenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataTransferThruNotificationCenter
+//
+//  Created by Marni Anuradha on 11/23/19.
+//  Copyright © 2019 Marni Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
